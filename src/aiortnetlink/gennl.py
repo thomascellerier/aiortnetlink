c7b4aee6d6774bf85c39eeecd56f03e6ec9f1242
Thomas Cellerier <thomascellerier@gmail.com>
2024-12-11 09:00:25 +0100

    Yet more enums

--- src/aiortnetlink/gennl.py
+++ src/aiortnetlink/gennl.py
@@ -3,6 +3,7 @@ Generic netlink
 """
 
 import struct
+from enum import IntEnum
 from typing import Final, Literal
 
 from aiortnetlink.netlink import (
@@ -13,30 +14,34 @@ from aiortnetlink.netlink import (
     NLAttr,
 )
 
-__all__ = ["get_family_request"]
+__all__ = ["get_family_request", "CtrlCmd"]
 
 
 GENL_ID_CTRL: Final = NLMSG_MIN_TYPE
 
-CTRL_CMD_UNSPEC: Final = 0
-CTRL_CMD_NEWFAMILY: Final = 1
-CTRL_CMD_DELFAMILY: Final = 2
-CTRL_CMD_GETFAMILY: Final = 3
-CTRL_CMD_NEWOPS: Final = 4
-CTRL_CMD_DELOPS: Final = 5
-CTRL_CMD_GETOPS: Final = 6
-CTRL_CMD_NEWMCAST_GRP: Final = 7
-CTRL_CMD_DELMCAST_GRP: Final = 8
-CTRL_CMD_GETMCAST_GRP: Final = 9  # unused
 
-CTRL_ATTR_UNSPEC: Final = 0
-CTRL_ATTR_FAMILY_ID: Final = 1
-CTRL_ATTR_FAMILY_NAME: Final = 2
-CTRL_ATTR_VERSION: Final = 3
-CTRL_ATTR_HDRSIZE: Final = 4
-CTRL_ATTR_MAXATTR: Final = 5
-CTRL_ATTR_OPS: Final = 6
-CTRL_ATTR_MCAST_GROUPS: Final = 7
+class CtrlCmd(IntEnum):
+    UNSPEC: Final = 0
+    NEWFAMILY: Final = 1
+    DELFAMILY: Final = 2
+    GETFAMILY: Final = 3
+    NEWOPS: Final = 4
+    DELOPS: Final = 5
+    GETOPS: Final = 6
+    NEWMCAST_GRP: Final = 7
+    DELMCAST_GRP: Final = 8
+    GETMCAST_GRP: Final = 9  # unused
+
+
+class CtrlAttr(IntEnum):
+    UNSPEC: Final = 0
+    FAMILY_ID: Final = 1
+    FAMILY_NAME: Final = 2
+    VERSION: Final = 3
+    HDRSIZE: Final = 4
+    MAXATTR: Final = 5
+    OPS: Final = 6
+    MCAST_GROUPS: Final = 7
 
 
 _GENMSGHDR_FMT: Final = (
@@ -58,8 +63,8 @@ def _genmsghdr(
 def get_family_request(family: str) -> NetlinkRequest:
     flags = NLM_F_REQUEST | NLM_F_ACK
     parts = [
-        _genmsghdr(CTRL_CMD_GETFAMILY, 1),
-        NLAttr.from_string(CTRL_ATTR_FAMILY_NAME, family),
+        _genmsghdr(CtrlCmd.GETFAMILY, 1),
+        NLAttr.from_string(CtrlAttr.FAMILY_NAME, family),
     ]
     data = b"".join(parts)
     return NetlinkRequest(GENL_ID_CTRL, flags, data, GENL_ID_CTRL)
